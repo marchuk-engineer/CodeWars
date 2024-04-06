debbb75553f9aa52df0029490f3b2074dbf615fe
Mykhailo Marchuk <nisha.mar4uck@gmail.com>
2024-04-06 19:01:45 +0300

    https://www.codewars.com/kata/reviews/633c94844d947c0001db30c5/groups/661171c09b442f00018329d3

---
+++ src/test/java/com/kata/training/VaporcodeTest.java
@@ -0,0 +1,14 @@
+package com.kata.training;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class VaporcodeTest {
+
+    @Test
+    public void vaporcode() {
+        assertEquals("L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S", Vaporcode.vaporcode("Lets go to the movies"));
+        assertEquals("W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G", Vaporcode.vaporcode("Why isnt my code working"));
+    }
+}
