36817cfe5b0dcb0ef5f7b9290f906ee725a48769
Mykhailo Marchuk <nisha.mar4uck@gmail.com>
2024-04-06 19:40:36 +0300

    JUnit library to 5th version

--- src/test/java/com/kata/training/SimpleBeadsCountTest.java
+++ src/test/java/com/kata/training/SimpleBeadsCountTest.java
@@ -1,7 +1,8 @@
 package com.kata.training;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // TODO: Replace examples and use TDD development by writing your own tests
 
@@ -10,14 +11,17 @@ public class SimpleBeadsCountTest {
     public void test0() {
         assertEquals(0, SimpleBeadsCount.countRedBeads(0));
     }
+
     @Test
     public void test1() {
         assertEquals(0, SimpleBeadsCount.countRedBeads(1));
     }
+
     @Test
     public void test3() {
         assertEquals(4, SimpleBeadsCount.countRedBeads(3));
     }
+
     @Test
     public void test5() {
         assertEquals(8, SimpleBeadsCount.countRedBeads(5));
